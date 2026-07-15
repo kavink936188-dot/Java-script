b5d41732ad4b7d16406ba13da2267addc305913d
Kavin <kavinsoftwareengineer@gmail.com>
2026-07-15 06:19:29 +0530

    FEAT:Long Number Division

---
+++ Variable's&Operation/addDiffNumbers.js
@@ -0,0 +1,8 @@
+let num1=10;
+letnum2=20;
+let num3=30;
+let num4=40;
+let num5=50;
+let num6=60;
+
+console.log(num1+num2+num3+num4+num5+num6);
---
+++ Variable's&Operation/findLongOfNumber.js
@@ -0,0 +1,5 @@
+let num1=4;
+let num2=6;
+let num3=9;
+
+console.log((num1+num2+num3)/3);
